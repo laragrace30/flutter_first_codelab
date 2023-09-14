6efe1b480c36a7c78a722e988ae8af86f97c4b4d
Lara Grace Riconalla <laragrace.riconalla.20@usjr.edu.ph>
2023-09-14 22:09:04 +0800

    refactor generator  page to its own widget

---
+++ lib/page/generator_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../states/my_app_state.dart';
+import '../widgets/big_card.dart';
+
+class GeneratorPage extends StatelessWidget {
+  const GeneratorPage({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var wordPair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(wordPair)){
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(wordPair: wordPair),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () => appState.toggleFavorite(),
+                 icon: Icon(icon), 
+                 label: const Text('Like'),
+                 ),
+                 const SizedBox(width: 10),  
+                ElevatedButton(
+                  onPressed: () => appState.getNext(),
+                  child: const Text('Next'),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,52 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../states/my_app_state.dart';
-import '../widgets/big_card.dart';
+import 'generator_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var wordPair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(wordPair)){
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BigCard(wordPair: wordPair),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () => appState.toggleFavorite(),
-                   icon: Icon(icon), 
-                   label: const Text('Like'),
-                   ),
-                   const SizedBox(width: 10),  
-                  ElevatedButton(
-                    onPressed: () => appState.getNext(),
-                    child: const Text('Next'),
-                    ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+    
+    return const SafeArea(
+        child: GeneratorPage(),
     );
   }
 }
+
+
 
