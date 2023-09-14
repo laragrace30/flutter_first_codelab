b5389b96849d426a0e427e5d317af1773945fb96
Lara Grace Riconalla <laragrace.riconalla.20@usjr.edu.ph>
2023-09-14 13:37:15 +0800

    create a BigCard widget with its own theme and style

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../states/my_app_state.dart';
+import '../widgets/big_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage ({super.key});
@@ -9,13 +10,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var wordPair = appState.current;
 
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: [
             const Text('A random idea:'),
-            Text(appState.current.asUpperCase),
+            BigCard(wordPair: wordPair),
             ElevatedButton(
               onPressed: () => appState.getNext(),
               child: const Text('Next'),
@@ -26,3 +28,4 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/widgets/big_card.dart
@@ -0,0 +1,28 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.wordPair,
+  });
+
+  final WordPair wordPair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textStyle = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(wordPair.asUpperCase, style: textStyle,
+        ),
+      ),
+      );
+  }
+}
